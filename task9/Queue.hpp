e9ade277f3777abecc52f469203e819a119b00b0
podkinpd <podkin.pd@gmail.com>
2020-12-15 08:04:26 +0100

    task9

--- task9/Queue.hpp
+++ task9/Queue.hpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <deque>
+#include <string>
 
-template<typename T, typename Out = std::ofstream()>
+template<typename T>
 class Queue {
  public:
  	Queue() : data_() {};
 
-	T& Front() const {
+	const T& Top() const {
 		return data_.front();
 	};
 
-	T& Back() const {
-		return data_.back();
-	};
-
-	void PushBack(T t) {
+	void Push(const T& t) {
 		data_.push_back(t);
 	};
 
-	void PushFront(T t) {
-		data_.push_front(t);
-	};
+	T Pop() {
+		T t = data_.front();
+		data_.pop_front();
+		return t;
+	}
 
 	void Print() {
 		for(int i = 0; i < data_.size(); i++) {
@@ -29,4 +28,33 @@ class Queue {
 	};
  protected:
  	std::deque<T> data_;
+};
+
+
+template<>
+class Queue<std::string> {
+ public:
+ 	Queue() : data_() {};
+
+	const std::string& Top() const {
+		return data_.front();
+	};
+
+	void Push(const std::string& t) {
+		data_.push_back(t);
+	};
+
+	std::string Pop() {
+		std::string s = data_.front();
+		data_.pop_front();
+		return s;
+	}
+
+	void Print() {
+		for(int i = 0; i < data_.size(); i++) {
+			std::cout << data_[i] << std::endl;
+		}
+	};
+ protected:
+ 	std::deque<std::string> data_;
 };
--- task9/test.cpp
+++ task9/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "ComplexNumber.hpp"
+#include "Queue.hpp"
 
 #include <fstream>
 
@@ -17,5 +18,18 @@ int main() {
 
 	fs.close();
 
+	Queue<int> q;
+	q.Push(1);
+	q.Push(2);
+	q.Push(3);
+	q.Print();
+	std::cout << std::endl << q.Pop() << std::endl << q.Top() << std::endl;
+	q.Print();
+
+	Queue<std::string> q1;
+	q1.Push("123");
+	q1.Push("234");
+	std::cout << q1.Top() << std::endl;
+	q1.Print();
 	return 0;
 };
